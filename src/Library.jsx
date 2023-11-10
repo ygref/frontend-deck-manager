6050e76e7bcf5b4ebc77c9d4c0c48f8d1c59c220
ygref <jason.wesley.hc@gmail.com>
2023-11-09 22:31:25 -0500

    fixed redundancy, dropdowns, quantity

--- src/Library.jsx
+++ src/Library.jsx
@@ -5,6 +5,10 @@ import "./Library.css";
 export function Library(props) {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState(null);
+  const [quantity, setQuantity] = useState(0);
+  const handleQuantityChange = (value) => {
+    setQuantity(value);
+  };
 
   const handleSearch = async () => {
     const searchUrl = `https://api.scryfall.com/cards/search?q=${searchQuery}`;
@@ -21,6 +25,50 @@ export function Library(props) {
     }
   };
 
+  const handleAddCard = (card) => {
+    props.onCreateCard(
+      {
+        name: card.name,
+        card_type: card.type_line,
+        cmc: card.cmc,
+        power: card.power,
+        toughness: card.toughness,
+        quantity: card.quantity,
+        deck_id: card.deck_id,
+      },
+      () => (window.location.href = "/decks")
+    );
+  };
+
+  const renderCardDetails = (card) => (
+    <div>
+      <img src={card.image_uris.small} />
+      <p>Card Name: {card.name}</p>
+      <p>Mana Cost: {card.mana_cost}</p>
+      <p>Card Type: {card.type_line}</p>
+      <p>Oracle text: {card.oracle_text}</p>
+      <p>
+        Power and Toughness: {card.power}/{card.toughness}
+      </p>
+      <p>Loyalty: {card.loyalty || "N/A"}</p>
+      <p>Sets: {card.set_name}</p>
+      <p>Price USD: {card.prices && card.prices.usd ? card.prices.usd : "N/A"}</p>
+      <p>
+        <a href={card.purchase_uris}>Purchase on TCGPlayer</a>{" "}
+      </p>
+      <select onChange={(event) => (card.deck_id = event.target.value)}>
+        <option value="">- Select a Deck -</option>
+        {props.decks.map((deck) => (
+          <option key={deck.id} value={deck.id}>
+            {deck.name}
+          </option>
+        ))}
+      </select>
+      <input type="number" placeholder="quantity" onChange={(event) => (card.quantity = event.target.value)} />
+      <button onClick={() => handleAddCard(card)}>Add Card</button>
+    </div>
+  );
+
   return (
     <div className="search-results">
       {" "}
@@ -36,82 +84,12 @@ export function Library(props) {
             <div className="card" key={result.id}>
               {result.card_faces ? (
                 <div>
-                  {result.card_faces.map((card_faces, index) => (
-                    <div key={index}>
-                      <img src={card_faces.image_uris.small} />
-                      <p>Card Name: {card_faces.name}</p>
-                      <p>Mana Cost: {card_faces.mana_cost}</p>
-                      <p>Card Type: {card_faces.type_line}</p>
-                      <p>Oracle text: {card_faces.oracle_text}</p>
-                      <p>
-                        Power and Toughness: {card_faces.power}/{card_faces.toughness}
-                      </p>
-                      <p>Loyalty: {card_faces.loyalty || "N/A"}</p>
-                      <p>Sets: {card_faces.set_name || result.set_name || "N/A"}</p>
-                      <p>
-                        Price USD:{" "}
-                        {card_faces.prices && card_faces.prices.usd
-                          ? card_faces.prices.usd
-                          : result.prices && result.prices.usd
-                          ? result.prices.usd
-                          : "N/A"}
-                      </p>
-                      <p>
-                        <a href={card_faces.purchase_uris || result.purchase_uris || "N/A"}>Purchase on TCGPlayer</a>{" "}
-                      </p>
-                    </div>
+                  {result.card_faces.map((cardFaces, index) => (
+                    <div key={index}>{renderCardDetails(cardFaces)}</div>
                   ))}
                 </div>
               ) : (
-                <div>
-                  <img src={result.image_uris.small} />
-                  <p>Card Name: {result.name}</p>
-                  <p>Mana Cost: {result.mana_cost}</p>
-                  <p>Card Type: {result.type_line}</p>
-                  <p>Oracle text: {result.oracle_text}</p>
-                  <p>
-                    Power and Toughness: {result.power}/{result.toughness}
-                  </p>
-                  <p>Loyalty: {result.loyalty || "N/A"}</p>
-                  <p>Sets: {result.set_name}</p>
-                  <p>Price USD: {result.prices.usd}</p>
-                  <p>
-                    <a href={result.purchase_uris}>Purchase on TCGPlayer</a>{" "}
-                  </p>
-
-                  <select onChange={(event) => (result.deck_id = event.target.value)}>
-                    <option value="">- Select a Deck -</option>
-                    {props.decks.map((deck) => (
-                      <option key={deck.id} value={deck.id}>
-                        {deck.name}
-                      </option>
-                    ))}
-                  </select>
-                  <input
-                    type="number"
-                    placeholder="quantity"
-                    value={result.quantity || 0}
-                    onChange={(event) => (result.quantity = event.target.value)}
-                  />
-                  <button
-                    onClick={() =>
-                      props.onCreateCard(
-                        {
-                          name: result.name,
-                          card_type: result.type_line,
-                          cmc: result.cmc,
-                          power: result.power,
-                          toughness: result.toughness,
-                          quantity: result.quantity,
-                          deck_id: result.deck_id,
-                        },
-                        () => console.log("Done!")
-                      )
-                    }
-                  >
-                    Add Card
-                  </button>
-                </div>
+                <div>{renderCardDetails(result)}</div>
               )}
             </div>
           ))}
